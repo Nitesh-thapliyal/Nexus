f9adc187274dd6c14ba3aa3c2d59f919ce035015
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2024-07-18 20:56:42 +0530

    update jwtTokenValidator

---
+++ src/main/java/com/nitesh/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.nitesh.config;
+
+public class JwtConstant {
+
+    public static final String SECRETE_KEY= "asdasdaswgwdasd";
+    public static final String JWT_HEADER= "Authorization";
+}
