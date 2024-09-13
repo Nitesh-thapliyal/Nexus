b11850f8092ec989c194ab9d171ff1d3c4bc1a15
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2024-09-14 01:20:49 +0530

    upd pro dtl issue list

--- Nexus-frontend/src/pages/ProjectDetail/ProjectDetails.jsx
+++ Nexus-frontend/src/pages/ProjectDetail/ProjectDetails.jsx
@@ -4,7 +4,9 @@ import { Button } from "@/components/ui/button"
 import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTrigger } from "@/components/ui/dialog"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { PlusIcon } from "@radix-ui/react-icons"
+import ChatBox from "./ChatBox"
 import InviteUserForm from "./InviteUserForm"
+import IssueList from "./IssueList"
 
 
 const ProjectDetails = () => {
@@ -19,8 +21,8 @@ const ProjectDetails = () => {
           <ScrollArea className="h-screen lg:w-[69%] pr-2">
             <div className="text-gray-400 pb-10 w-full">
               <h1 className="text-lg font-semibold pb-5">Create SpaceX for India</h1>
-              <div className="space-y-5 pb-10">
-              <p className="w-full md:max-w-lg lg:max-w-xl">
+              <div className="space-y-5 pb-10 text-sm">
+              <p className="w-full md:max-w-lg lg:max-w-xl ">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </p>
 
@@ -65,8 +67,23 @@ const ProjectDetails = () => {
               </div>
 
             </div>
+
+            <section>
+              <p className="py-5 border-b text-lg -trackign-wider">Tasks</p>
+              <div className="lg:flex md:flex gap-3 justify-between py-5">
+                <IssueList status="pending" title="Todo List"/>
+                <IssueList status="in_progress" title="In Progress"/>
+                <IssueList status="done" title="Done"/>
+              </div>
+            </section>
+
             </div>
           </ScrollArea>
+
+          <div>
+            <ChatBox/>
+          </div>
+
         </div>
       </div>
     </>
