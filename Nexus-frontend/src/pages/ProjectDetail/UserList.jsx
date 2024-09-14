f868f9ac2dfbb42bf267e8941bb4dfca1576e052
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2024-09-14 12:12:34 +0530

    upd issue and user lst

--- Nexus-frontend/src/pages/ProjectDetail/UserList.jsx
+++ Nexus-frontend/src/pages/ProjectDetail/UserList.jsx
@@ -1,6 +1,27 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar"
+
 const UserList = () => {
   return (
-    <div>UserList</div>
+    <>
+        <div className="space-y-2">
+            <div className="border rounded-md">
+                <p className="py-2 px-3">{"nitesh" || "Unasignee"}</p>
+            </div>
+
+            {[1,1,1,1].map((item) => <div key={item} className="py-2 group hover:bg-slate-800 cursor-pointer flex items-center space-x-4 rounded-md border px-4">
+                <Avatar>
+                    <AvatarFallback>
+                        N
+                    </AvatarFallback>
+                </Avatar>
+                <div className="space-y-1">
+                    <p className="text-sm leading-none">@Nitesh Thapliyal</p>
+                    <p className="text-sm text-muted-foreground">NiteshThapliyal</p>
+                </div>
+            </div> )}
+        </div>
+    
+    </>
   )
 }
 
