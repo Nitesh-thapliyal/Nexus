2c4a01c982669d9eb2af13c914f50922e21e22b3
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2024-09-18 18:45:34 +0530

    add auth page with css

--- Nexus-frontend/src/App.jsx
+++ Nexus-frontend/src/App.jsx
@@ -1,26 +1,27 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from "./pages/Home/Home"
-import IssuseDetails from './pages/IssueDetails/IssuseDetails'
-import Navbar from './pages/Navbar/Navbar'
-import ProjectDetails from './pages/ProjectDetail/ProjectDetails'
-import Subscription from './pages/Subscription/Subscription'
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Auth from "./pages/Auth/Auth";
+import Home from "./pages/Home/Home";
+import IssuseDetails from "./pages/IssueDetails/IssuseDetails";
+import Navbar from "./pages/Navbar/Navbar";
+import ProjectDetails from "./pages/ProjectDetail/ProjectDetails";
+import Subscription from "./pages/Subscription/Subscription";
 
 function App() {
-
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/project/:id" element={<ProjectDetails/>}/>
-      <Route path="/project/:projectId/issue/:issueId" element={<IssuseDetails/>}/>
-      <Route path="/upgrade_plan" element={<Subscription/>}/>
-    </Routes>
- 
+      { false ? <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project/:id" element={<ProjectDetails />} />
+          <Route path="/project/:projectId/issue/:issueId" element={<IssuseDetails />}/>
+          <Route path="/upgrade_plan" element={<Subscription />} />
+        </Routes>
+      </div> : <Auth/>
+      }
     </>
-  )
+  );
 }
 
-export default App
+export default App;
