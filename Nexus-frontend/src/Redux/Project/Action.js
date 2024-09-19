829110a0133f790d7109dbc5259e65065c66220b
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2024-09-19 19:27:40 +0530

    upd redux cfg

--- Nexus-frontend/src/Redux/Project/Action.js
+++ Nexus-frontend/src/Redux/Project/Action.js
@@ -1,5 +1,5 @@
 import api from "@/config/api";
-import { FETCH_PROJECTS_BY_ID_REQUESTS, FETCH_PROJECTS_BY_ID_SUCCESS } from "./ActionTypes";
+import { CREATE_PROJECTS_REQUESTS, CREATE_PROJECTS_SUCCESS, DELETE_PROJECTS_REQUESTS, DELETE_PROJECTS_SUCCESS, FETCH_PROJECTS_BY_ID_REQUESTS, FETCH_PROJECTS_BY_ID_SUCCESS, SEARCH_PROJECTS_REQUESTS, SEARCH_PROJECTS_SUCCESS } from "./ActionTypes";
 
 
 export const fetchProjects = ({category, tag}) => async (dispatch) => {
@@ -7,7 +7,52 @@ export const fetchProjects = ({category, tag}) => async (dispatch) => {
     try {
         const {data} = await api.get("/api/projects", {params:{category,tag}})
         console.log("all projects", data);
-        dispatch({type: FETCH_PROJECTS_BY_ID_SUCCESS})
+        dispatch({type: FETCH_PROJECTS_BY_ID_SUCCESS, projects:data})
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const searchProjects = ({keyword}) => async (dispatch) => {
+    dispatch({type: SEARCH_PROJECTS_REQUESTS})
+    try {
+        const {data} = await api.get("/api/projects/search?keyword="+keyword)
+        console.log("Search projects", data);
+        dispatch({type: SEARCH_PROJECTS_SUCCESS, projects:data})
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const createProjects = (projectData) => async (dispatch) => {
+    dispatch({type: CREATE_PROJECTS_REQUESTS})
+    try {
+        const {data} = await api.post("/api/projects", projectData)
+        console.log("Search projects", data);
+        dispatch({type: CREATE_PROJECTS_SUCCESS, projects:data})
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const fetchProjectById = (id) => async (dispatch) => {
+    dispatch({type: FETCH_PROJECTS_BY_ID_REQUESTS})
+    try {
+        const {data} = await api.get("/api/projects/"+id)
+        console.log("projects", data);
+        dispatch({type: FETCH_PROJECTS_BY_ID_SUCCESS, projects:data})
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const deleteProjectById = (projectId) => async (dispatch) => {
+    dispatch({type: DELETE_PROJECTS_REQUESTS})
+    try {
+        const {data} = await api.delete("/api/projects/"+projectId)
+        console.log("delete projects", data);
+        dispatch({type: DELETE_PROJECTS_SUCCESS, projectId})
     } catch (error) {
         console.log(error);
     }
